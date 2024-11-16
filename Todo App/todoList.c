286c41403a8c092326559e991985727e792d5592
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-11-16 11:33:13 -0500

    Create todo list ADT

---
+++ Todo App/todoList.c
@@ -0,0 +1,78 @@
+// James Payne, Brandon Carr-Ansah, Nifemi Olapoju - prog71990 - group assignment - fall24
+
+#include "todoList.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+bool addTodoItem(ptodo_list_t* list, todo_item_t item) {
+    if (!list) {
+        return false;
+    }
+
+    todo_list_t* newNode = (todo_list_t*)malloc(sizeof(todo_list_t));
+    if (!newNode) {
+        fprintf(stderr, "Failed to allocate new todo list node.");
+        return false;
+    }
+
+    newNode->item = copyTodoItem(item);
+    newNode->next = *list;
+    *list = newNode;
+
+    return true;
+}
+
+bool removeTodoItem(ptodo_list_t* list, todo_item_t item) {
+    if (!list || !*list) {
+        return false;
+    }
+
+    // Check list head
+    ptodo_list_t current = *list;
+    if (compareTodoItems(current->item, item)) {
+        destroyTodoItem(current->item);
+
+        *list = current->next;
+        free(current);
+        
+        return true;
+    }
+
+    // Check rest of list
+    ptodo_list_t prev = current;
+    current = current->next;
+    while (current != NULL) {
+        if (compareTodoItems(current->item, item)) {
+            prev->next = current->next;
+
+            destroyTodoItem(current->item);
+            free(current);
+
+            return true;
+        }
+
+        prev = current;
+        current = current->next;
+    }
+
+    return false;
+}
+
+void clearTodoList(ptodo_list_t* list) {
+    if (!list) {
+        return;
+    }
+
+    destroyTodoList(*list);
+    *list = NULL;
+}
+
+void destroyTodoList(ptodo_list_t list) {
+    ptodo_list_t current = list;
+    while (current != NULL) {
+        ptodo_list_t temp = current;
+        current = temp->next;
+        free(temp);
+    }
+}
---
+++ Todo App/todoList.h
@@ -0,0 +1,18 @@
+// James Payne, Brandon Carr-Ansah, Nifemi Olapoju - prog71990 - group assignment - fall24
+
+#pragma once
+
+#include "todoItem.h"
+
+typedef struct todo_list {
+    todo_item_t item;
+    struct todo_list* next;
+} todo_list_t, *ptodo_list_t;
+
+bool addTodoItem(ptodo_list_t* list, todo_item_t item);
+
+bool removeTodoItem(ptodo_list_t* list, todo_item_t item);
+
+void clearTodoList(ptodo_list_t* list);
+
+void destroyTodoList(ptodo_list_t list);
