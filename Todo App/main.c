21f181bbd2a09c6905b103a80b9f95402d836114
ModularMemory <182370532+ModularMemory@users.noreply.github.com>
2024-11-16 09:11:30 -0500

    Create todo app project

---
+++ Todo App/main.c
@@ -0,0 +1,7 @@
+// James Payne, Brandon Carr-Ansah, Nifemi Olapoju - prog71990 - group assignment - fall24
+
+#include "main.h"
+
+int main(void) {
+    return 0;
+}
---
+++ Todo App/main.h
@@ -0,0 +1,5 @@
+// James Payne, Brandon Carr-Ansah, Nifemi Olapoju - prog71990 - group assignment - fall24
+
+#pragma once
+
+int main(void);
